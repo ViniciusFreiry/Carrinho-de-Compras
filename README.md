4297c44c3211a0127840faf562100682bfcd5265
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-19 16:44:42 -0300

    Adding README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Carrinho-de-Compras
+# 🛒 Carrinho de Compras - JavaScript
+
+Este projeto é uma simulação de um carrinho de compras interativo utilizando **JavaScript puro**, com HTML e CSS previamente implementados.
+
+## ✨ Funcionalidades
+
+- Adição dinâmica de produtos ao carrinho.
+- Cálculo automático do valor total com base na quantidade e no preço dos produtos.
+- Limpeza automática do carrinho ao iniciar a página ou após pressionar o botão de limpar.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const listaProdutos = document.getElementById("lista-produtos");
 const total = document.getElementById("valor-total");
+let totalCarrinho = 1400;
+limpar();
 
 function adicionar() {
     // Recuperando Informações do Produto
@@ -8,21 +10,25 @@ function adicionar() {
     const valorProduto = produto.split("$")[1];
     const quantidade = document.getElementById("quantidade").value;
 
-    // Ajustando Preço e Subtotal
+    if (quantidade <= 0 || quantidade == "") return 0;
+
+    // Adicionando Produto ao Carrinho
     const preco = valorProduto * quantidade;
-    let totalCarrinho = parseInt(total.innerHTML.split("$")[1]);
 
     listaProdutos.innerHTML += `
         <section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto}<span class="texto-azul">R$${preco}</span>
         </section>`;
 
+    // Ajustando Preço e Subtotal do Carrinho
     total.innerHTML = `R$${totalCarrinho + preco}`;
+    totalCarrinho += preco;
+
+    document.getElementById("quantidade").value = "";
 }
 
 function limpar() {
     listaProdutos.innerHTML = "";
     total.innerHTML = "R$0"
+    totalCarrinho = 0;
 }
-
-limpar();
